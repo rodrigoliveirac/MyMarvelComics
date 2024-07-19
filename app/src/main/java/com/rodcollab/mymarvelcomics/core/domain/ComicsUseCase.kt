3e05907833c9de37d44c87124e3f7bde6652e825
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-07-19 04:07:58 -0300

    [MMC-04] - mapping favorite items

--- app/src/main/java/com/rodcollab/mymarvelcomics/core/domain/ComicsUseCase.kt
+++ app/src/main/java/com/rodcollab/mymarvelcomics/core/domain/ComicsUseCase.kt
@@ -5,13 +5,40 @@ import androidx.paging.map
 import com.rodcollab.mymarvelcomics.core.data.model.toComic
 import com.rodcollab.mymarvelcomics.core.data.repository.ComicsRepository
 import com.rodcollab.mymarvelcomics.core.model.Comic
+import com.rodcollab.mymarvelcomics.core.utils.ResultOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 class ComicsUseCaseImpl (private val comicsRepository: ComicsRepository):
     ComicsUseCase {
     override fun invoke(): Flow<PagingData<Comic>> {
-        return comicsRepository.getPagingComics(15).flow.map { it.map { it.toComic() } }
+        return comicsRepository.getPagingComics(15).flow.map {
+            it.map { comicEntity ->
+                withContext(Dispatchers.IO) {
+                    val call: suspend () -> Comic = {
+                        async {
+                            var resultComic: Comic? = null
+                            comicsRepository.readFavoriteComic(comicEntity.id) {
+                                resultComic = when (it) {
+                                    is ResultOf.Success -> {
+                                        it.value
+                                    }
+
+                                    is ResultOf.Failure -> {
+                                        comicEntity.toComic()
+                                    }
+                                }
+                            }
+                            resultComic!!
+                        }.await()
+                    }
+                    call()
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/rodcollab/mymarvelcomics/core/domain/GetComicsByCharId.kt
+++ app/src/main/java/com/rodcollab/mymarvelcomics/core/domain/GetComicsByCharId.kt
@@ -5,13 +5,40 @@ import androidx.paging.map
 import com.rodcollab.mymarvelcomics.core.data.model.toComic
 import com.rodcollab.mymarvelcomics.core.data.repository.ComicsRepository
 import com.rodcollab.mymarvelcomics.core.model.Comic
+import com.rodcollab.mymarvelcomics.core.utils.ResultOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
-class GetComicsByCharIdImpl (private val comics: ComicsRepository) :
+class GetComicsByCharIdImpl(private val comics: ComicsRepository) :
     GetComicsByCharId {
     override fun invoke(charId: Int): Flow<PagingData<Comic>> {
-        return comics.getPagingComicsByCharId(15, charId).flow.map { it.map { it.toComic() } }
+        return comics.getPagingComicsByCharId(15, charId).flow.map {
+            it.map { comicEntity ->
+                withContext(Dispatchers.IO) {
+                    val call: suspend () -> Comic = {
+                        async {
+                            var resultComic: Comic? = null
+                            comics.readFavoriteComic(comicEntity.id) {
+                                resultComic = when (it) {
+                                    is ResultOf.Success -> {
+                                        it.value
+                                    }
+
+                                    is ResultOf.Failure -> {
+                                        comicEntity.toComic()
+                                    }
+                                }
+                            }
+                            resultComic!!
+                        }.await()
+                    }
+                    call()
+                }
+            }
+        }
     }
 }
 
